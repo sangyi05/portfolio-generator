6c3b2d43b0cce76af77f1bd4497aa9f228408da5
Helen <sang.s.yi@gmail.com>
2021-02-02 22:52:11 -0800

    generate html

--- app.js
+++ app.js
@@ -1,15 +1,27 @@
-const profileDataArgs = process.argv.slice(2);
+const fs = require('fs')
+const generatePage = require('./src/page-template.js')
+const profileDataArgs = process.argv.slice(2)
+const [name, github] = profileDataArgs;
 
-const printProfileData = profileDataArr => {
-  // This...
-  for (let i = 0; i < profileDataArr.length; i += 1) {
-    console.log(profileDataArr[i]);
-  }
+fs.writeFile('./index.html', generatePage(name, github), err => {
+    if (err) throw new Error(err);
+    console.log('Portfolio complete! Checkout index.html to see the output!')
+})
 
-  console.log('================');
+// const name = profileDataArgs[0]
+// const github = profileDataArgs[1]  THIS IS THE SAME THING AS const [name, github] = profileDataArgs
 
-  // Is the same as this...
-  profileDataArr.forEach(profileItem => console.log(profileItem));
-};
+// const printProfileData = profileDataArr => {
+//   // This...
+//   for (let i = 0; i < profileDataArr.length; i += 1) {
+//     console.log(profileDataArr[i]);
+//   }
 
-printProfileData(profileDataArgs);
+//   console.log('================');
+
+//   // Is the same as this...
+//   profileDataArr.forEach(profileItem => console.log(profileItem));
+// };
+
+// printProfileData(profileDataArgs);
+
